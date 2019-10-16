98f5d67effa074005ac458e1f52cae645823c9c6
laiso <laiso@lai.so>
2019-10-16 20:41:04 +0700

    dummy test

--- todoTests/todoTests.swift
+++ todoTests/todoTests.swift
@@ -19,8 +19,7 @@ class todoTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(1+1, 2)
     }
 
     func testPerformanceExample() {
